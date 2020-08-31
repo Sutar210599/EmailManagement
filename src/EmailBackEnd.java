961ad83770f3ea75355e67bcceb5847ac1088c21
Sutar210599 <gouravsutar@gmail.com>
2020-09-01 00:38:50 +0530

    Emails are in lower case

--- src/EmailBackEnd.java
+++ src/EmailBackEnd.java
@@ -56,7 +56,7 @@ public class EmailBackEnd {
 
     }
     private String createEmail(){
-        String email=this.firstName+"."+this.lastName+"@"+companyName+"-"+this.department+".com";
+        String email=this.firstName.toLowerCase()+"."+this.lastName.toLowerCase()+"@"+companyName+"-"+this.department+".com";
         return email;
 
     }
