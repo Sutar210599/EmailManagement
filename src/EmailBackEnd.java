7d3e7d963021315b6e635b3963d2065445e42dbc
Sutar210599 <gouravsutar@gmail.com>
2020-09-01 00:04:00 +0530

    Update EmailBackEnd.java

--- src/EmailBackEnd.java
+++ src/EmailBackEnd.java
@@ -3,5 +3,28 @@ public class EmailBackEnd {
     private String lastName;
     private String department;
     private String password;
-    
+    private String emailid;
+
+    EmailBackEnd(String firstName, String LastName){
+        this.firstName=firstName;
+        this.lastName=LastName;
+        this.department=setDepartment();
+        this.password=setPass();
+        this.emailid=createEmail();
+
+    }
+
+    private String setDepartment(){
+        System.out.println("Choose your Department :\n1.Sales\n2.DevOps\n3.Management\n ");
+        
+
+    }
+
+    private String setPass(){
+
+    }
+    private String createEmail(){
+
+    }
+
 }
