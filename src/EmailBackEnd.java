cb33c3bb8e65551ca74966430b46cd64641dafa1
Sutar210599 <gouravsutar@gmail.com>
2020-09-01 14:15:48 +0530

    Final Changes

--- src/EmailBackEnd.java
+++ src/EmailBackEnd.java
@@ -8,44 +8,55 @@ public class EmailBackEnd {
     private String companyName="Astril";
     private String admin="Sutar";
     private String admin_password="sutar#210599";
+    private boolean runtime=true;
     private HashMap<String, String> email_pass=new HashMap<>();//Maintain a record of Employee credentials
 
     EmailBackEnd(String adminID, String adminPass){
+
         if (validateAdmin(adminID,adminPass)){
-            System.out.println("Welcome Admin!. Press 1 for adding employee, Press 2 for management employee");
-            Scanner ad=new Scanner(System.in);
-            int adch=ad.nextInt();
+            while (this.runtime){
+                System.out.println("Welcome Admin! Press 1 for adding employee, Press 2 for management employee, Press 3 to exit");
+                Scanner ad=new Scanner(System.in);
+                int adch=ad.nextInt();
 
-            if (adch==1){
-                System.out.println("Enter the number of employees you wish to add.");
-                int num=ad.nextInt();
-                for (int k=0;k<num;k++){
-                    System.out.println("Enter employee "+(k+1) +" first name :");
-                    String first=ad.next();
-                    System.out.println("Enter employee " +(k+1)+" last name");
-                    String last=ad.next();
-                    setemp(first,last);
+                if (adch==1){
+                    System.out.println("Enter the number of employees you wish to add.");
+                    int num=ad.nextInt();
+                    for (int k=0;k<num;k++){
+                        System.out.println("Enter employee "+(k+1) +" first name :");
+                        String first=ad.next();
+                        System.out.println("Enter employee " +(k+1)+" last name");
+                        String last=ad.next();
+                        setemp(first,last);
+                    }
                 }
-            }
-            else if (adch==2){
-                System.out.println("Press 1 to view the hashmap of credentials, Press 2 to change password for an employee");
-                int d=ad.nextInt();
-                if (d==1){
-                    System.out.println(email_pass);
-                }
-                else if (d==2){
-                    System.out.println("Enter the email id of the employee");
-                    String emailChange=ad.next();
-                    if (email_pass.containsKey(emailChange)){
-                        System.out.println("Employee found!");
-                        System.out.println("Generating a new password for employee");
-                        String newPass=setPass();
-                        System.out.println("The new password is "+newPass);
+                else if (adch==2){
+                    System.out.println("Press 1 to view the hashmap of credentials, Press 2 to change password for an employee");
+                    int d=ad.nextInt();
+                    if (d==1){
+                        System.out.println(email_pass);
+                    }
+                    else if (d==2) {
+                        System.out.println("Enter the email id of the employee");
+                        String emailChange = ad.next();
+                        if (email_pass.containsKey(emailChange)) {
+                            System.out.println("Employee found!");
+                            System.out.println("Generating a new password for employee");
+                            String newPass = setPass();
+                            System.out.println("The new password is " + newPass);
 
+                        } else {
+                            System.out.println("Email id does not exist");
+                        }
                     }
-                    else{
-                        System.out.println("Email id does not exist");
-                    }
+                else if(adch==3){
+                    System.out.println("Bye!");
+                    this.runtime=false;
+                    break;
+
+                        }
+
+
                 }
             }
 
